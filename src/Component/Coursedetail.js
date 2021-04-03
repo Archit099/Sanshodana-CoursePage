17529a207c377ce15fe45976fa83f43b36ca89e7
Archit099 <you@example.com>
2021-04-03 13:13:08 -0600

    course

---
+++ src/Component/Coursedetail.js
@@ -0,0 +1,250 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+
+function Coursedetail() {
+    return (
+        <div style={{backgroundColor:'#eee'}}>
+        <Container>
+          
+        <Row style={{backgroundColor:'white'}}>
+         <Col lg={8}>
+           <div style={{marginTop:'3%'}}>
+            <h2>Courese Title</h2>
+          <p style={{paddingLeft:'1%'}}>course detail</p>
+          </div>
+          <hr/>
+          <div style={{marginTop:'3%',backgroundColor:'white'}}>
+            <h4>Discription</h4>
+          <p style={{paddingLeft:'1%'}}>The main appearance of blockchain innovation rose in 2009 with the Bitcoin blockchain, a safe, control safe, shared electronic money framework. Since Bitcoin is available to anybody, it is a case of an open, or a permission-less blockchain.
+
+          The Blockchain Essence course will brace you with all key concepts of blockchain and make you well versed in the overall idea of the technology- its past and the present.</p>
+          </div>
+
+          
+          <div style={{marginTop:'3%',backgroundColor:'white'}}>
+            <h4>Discription</h4>
+          <p style={{paddingLeft:'1%'}}>The main appearance of blockchain innovation rose in 2009 with the Bitcoin blockchain, a safe, control safe, shared electronic money framework. Since Bitcoin is available to anybody, it is a case of an open, or a permission-less blockchain.
+
+          The Blockchain Essence course will brace you with all key concepts of blockchain and make you well versed in the overall idea of the technology- its past and the present.</p>
+          </div>
+          <Row>
+          <Col lg={6}>
+          <div style={{marginTop:'3%'}}>
+          <h4>Features</h4>
+        <p>
+        <ul style={{listStyleType: 'none',paddingLeft:'0%',lineHeight:'200%'}}>
+         <li>
+           ✔️ feature1
+         </li>
+         <li>
+         ✔️ feature1
+         </li>
+         <li>
+         ✔️ feature1
+         </li>
+         <li>
+         ✔️ feature1
+         </li>
+         </ul>
+        </p>
+        </div>
+          </Col>
+          <Col lg={6}>
+          <div style={{marginTop:'3%'}}>
+          <h4>Presiquite</h4>
+        <p>
+        <ul style={{listStyleType: 'none',paddingLeft:'0%',lineHeight:'200%'}}>
+         <li>
+         ✔️ feature1
+         </li>
+         <li>
+         ✓ feature1
+         </li>
+         <li>
+         ✔️ feature1
+         </li>
+         <li>
+         ✔️ feature1
+         </li>
+         </ul>
+        </p>
+        </div>
+          </Col>
+          </Row>
+          
+         <Card style={{marginBottom:'2%'}}>
+         <ListGroup variant="flush" >
+           <ListGroup.Item >
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>  
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+           
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+
+           <ListGroup.Item>
+           <Row>
+           <Col lg={8} xs={8}>
+           <h5><div className="courses-image"><a href="https://courses.learncodeonline.in/" className="d-block image" style={{textDecoration:'none', color:'black'}}>▶️ Cras justo odio</a></div></h5>
+           </Col>
+           <Col lg={4} xs={4}>
+           <h5 style={{marginLeft:'20%'}}>Hello</h5>
+           </Col>
+           </Row>
+           
+           </ListGroup.Item>
+           
+         </ListGroup>
+       </Card>
+         </Col>
+
+         <Col lg={4} style={{marginTop:"5%"}}>
+         <Card sticky="top" style={{marginRight:"2%"}}>
+  <Card.Img variant="top" src="/logo192.png" height='300px'/>
+  <Card.Body>
+  
+    <Card.Title><h4> $250</h4></Card.Title>
+    
+    <Card.Text>
+    <div style={{marginTop:'3%'}}>
+    <h4>Features</h4>
+  <p>
+  <ul style={{listStyleType: 'none',paddingLeft:'0%',lineHeight:'200%'}}>
+   <li>
+     ✔️ feature1
+   </li>
+   <li>
+   ✔️ feature1
+   </li>
+   <li>
+   ✔️ feature1
+   </li>
+   
+   </ul>
+  </p>
+  </div>
+  
+  <Button variant="outline-dark" block="true"> Enroll Now</Button>
+  
+    </Card.Text>
+  </Card.Body>
+ 
+  
+</Card>
+
+         </Col>
+        </Row>
+        </Container>
+        </div>
+    )
+}
+
+export default Coursedetail
